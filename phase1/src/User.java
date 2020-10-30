171544a812fa3c1af3ad6d92c2dba90bb7e05005
FredS <fred.sallah@mail.utoronto.ca>
2020-10-30 13:26:44 -0400

    Updated entities to align with Phase1 design.

--- phase1/src/User.java
+++ phase1/src/User.java
@@ -1,11 +1,13 @@
 import java.util.ArrayList;
 
 public abstract class User{
-    public String name, userName = null;
+    private String name, userName = null;
 
     private char[] hashPassword = null;
 
     private ArrayList<String> events = null;
+
+
 
 }
 
