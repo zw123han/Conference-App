3ccdc03c7ddf512c6ed0e05f4762c9cf147b7540
Jesse Na <jessena.2001@gmail.com>
2020-11-03 14:12:17 -0500

    Reorganize inheritance hierarchy and implement organizer

--- phase1/src/Organizer.java
+++ phase1/src/Organizer.java
@@ -1,2 +1,13 @@
 public class Organizer extends User {
+
+    public isAttendee() { return false; }
+
+    public isOrganizer() {
+        return true;
+    }
+
+    public isSpeaker() {
+        return false;
+    }
+
 }
--- phase1/src/Speaker.java
+++ phase1/src/Speaker.java
@@ -2,17 +2,21 @@ import java.util.ArrayList;
 
 public class Speaker extends User {
 
-    private ArrayList<String> events = null;
+    //private ArrayList<Event> talks;
 
-    private ArrayList<String> talks = null;
+    //public ArrayList<Event> getTalks(){ return talks; } // return list of talks this user is giving
 
-    public ArrayList<String> getEvents(){ return events; } //return list of events this user is attending.
+    //public void setTalks(String talk){ talks.add(talk); }
 
-    public void setEvents(String event){ events.add(event); }
+    public isAttendee() { return false; }
 
-    public ArrayList<String> getTalks(){ return talks; } // return list of talks this user is giving
+    public isOrganizer() {
+        return false;
+    }
 
-    public void setTalks(String talk){ talks.add(talk); }
+    public isSpeaker() {
+        return true;
+    }
 
 
 }
