856d8e728472ad690db33ffed4c1b541ec4faeaf
Elliot <elliot.chen@mail.utoronto.ca>
2020-11-03 20:43:49 -0800

    Added to Chatroom

--- phase1/src/Chatroom.java
+++ phase1/src/Chatroom.java
@@ -1,9 +1,16 @@
+import java.lang.reflect.Array;
 import java.util.ArrayList;
 
 public class Chatroom {
     private ArrayList<User> users;
+    private ArrayList<Message> history;
 
     public Chatroom(ArrayList<User> users){
-        this.users = users;
+        this.users = Array.sort(users);
+        this.history = new ArrayList<Message>();
+    }
+
+    public void sendMessage(Message message){
+        history.add(message);
     }
 }
