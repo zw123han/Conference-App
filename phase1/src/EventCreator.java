cbcbff6675dfe55b9203e3725621c25a9c7cb70b
NithilanManimaran <56126201+NithilanManimaran@users.noreply.github.com>
2020-11-03 21:12:09 -0500

    EventManager and eventcreator intial completed

---
+++ phase1/src/EventCreator.java
@@ -0,0 +1,23 @@
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+
+public class EventCreator {
+    private EventManager em;
+
+    public EventCreator(EventManager em) {
+        this.em = em;
+    }
+
+    public boolean createEvent(String name, String room, LocalDateTime time, Speaker speaker) {
+        ArrayList<Event> events = this.em.getEvents();
+        for (Event event : events) {
+            if ((event.getRoom() == room && event.getTime() == time) ||
+                    (event.getTime() == time && event.getSpeaker().getUserName() == speaker.getUserName())) {
+                return false;
+            }
+        }
+        Event ev = new Event(name, room, time, speaker);
+        this.em.createEvent(name, room, time, speaker);
+        return true;
+    }
+}
--- phase1/src/EventManager.java
+++ phase1/src/EventManager.java
@@ -1,26 +1,24 @@
 import java.util.*;
 import java.time.*;
-public class EventManager{
+
+public class EventManager {
 
     private ArrayList<Event> events;
 
-    public EventManager(ArrayList<Event> events){
+    public EventManager(ArrayList<Event> events) {
         this.events = events;
     }
 
-    public EventManager(){
+    public EventManager() {
         this.events = new ArrayList<Event>();
     }
 
-    public boolean createEvent(String name, String room, LocalDateTime time, Speaker speaker){
-        for (event : this.events){
-            if ((event.getRoom() == room && event.getTime() == time) ||
-                    (event.getTime() == time && event.getSpeaker().getUserName() == speaker.getUserName())) {
-                return false
-            }
-        }
+    public void createEvent(String name, String room, LocalDateTime time, Speaker speaker) {
         Event ev = new Event(name, room, time, speaker);
         this.events.add(ev);
-        return true
+    }
+
+    public ArrayList<Event> getEvents() {
+        return this.events;
     }
 }
