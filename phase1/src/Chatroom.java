b70a2755e238340df2b8add8e50963810d040308
Elliot <elliot.chen@mail.utoronto.ca>
2020-11-03 21:10:57 -0800

    Made Chatroom an Observable (WIP)

--- phase1/src/Chatroom.java
+++ phase1/src/Chatroom.java
@@ -1,16 +1,21 @@
 import java.lang.reflect.Array;
 import java.util.ArrayList;
+import java.util.Observable;
 
-public class Chatroom {
+public class Chatroom extends Observable {
     private ArrayList<User> users;
     private ArrayList<Message> history;
 
     public Chatroom(ArrayList<User> users){
         this.users = Array.sort(users);
         this.history = new ArrayList<Message>();
+        for(User i : users){
+            addObserver(i);
+        }
     }
 
     public void sendMessage(Message message){
         history.add(message);
+        setChanged();
     }
 }
