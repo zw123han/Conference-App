11ab9298803d2184d4f742802b59844ab6a85f05
FredS <fred.sallah@mail.utoronto.ca>
2020-11-04 21:24:54 -0500

    Setting up main method for future use.

---
+++ phase1/src/ConferenceSimulator.java
@@ -0,0 +1,9 @@
+public class ConferenceSimulator {
+
+    public void run(){
+
+        //TBD
+
+    }
+
+}
---
+++ phase1/src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+
+    public static void main(String[] arg){
+
+        ConferenceSimulator conference = new ConferenceSimulator();
+
+        conference.run();
+
+    }
+
+}
