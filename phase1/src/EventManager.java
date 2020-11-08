7dcc1d03b9e3fdb778223ba4d26d671ad968d5d4
Chrisee Zhu <fishnuggets.zhu@mail.utoronto.ca>
2020-11-07 22:07:16 -0800

    removed getEventByID since event IDs have not been implemented yet and added canSendOne (incomplete) to CanIMessageThisPersonIDontKnowCanYou

--- phase1/src/EventManager.java
+++ phase1/src/EventManager.java
@@ -28,7 +28,4 @@ public class EventManager {
         return this.events;
     }
 
-    public Event getEventByID(String id) {
-        return events.get(id);
-    }
 }
--- phase1/src/canIMessageThisPersonIDontKnowCanYou.java
+++ phase1/src/canIMessageThisPersonIDontKnowCanYou.java
@@ -1,2 +1,20 @@
+import java.util.ArrayList;
+
 public class canIMessageThisPersonIDontKnowCanYou {
+    Chatter chatter = new Chatter();
+
+    public canIMessageThisPersonIDontKnowCanYou() {
+    }
+
+    public boolean canSendOne(User user, String recipient, Message message) {
+        if ((user instanceof Attendee || user instanceof Organizer) && user.getFriends().contains(recipient)) {
+            ArrayList<String> recipients = new ArrayList<>();
+            recipients.add(user.getUserName());
+            recipients.add(recipient);
+            chatter.sendOne(recipients, message);
+            return true;
+        }
+        return false;
+    }
+
 }
