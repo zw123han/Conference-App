e25154ce40ce5712e036f54c1e4b396b38c199b0
Weelam <wl.lin@mail.utoronto.ca>
2020-11-08 02:21:36 -0500

    removed block of code in EventSignupPresenter

--- phase1/src/EventSignupPresenter.java
+++ phase1/src/EventSignupPresenter.java
@@ -1,16 +1,3 @@
 public class EventSignupPresenter {
 
-    private EventSignupController esc;
-
-    public EventSignupPresenter(EventSignupController esc) {
-        this.esc = esc;
-    }
-
-    public boolean joinEvent(String event_name, User user) {
-        return esc.signUserUp(user, event_name);
-    }
-
-    public boolean leaveEvent(String event_name, User user) {
-        return esc.removeUser(user, event_name);
-    }
 }
