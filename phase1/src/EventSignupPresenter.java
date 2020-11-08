9f36e5f104014d61ec2119967e9508421671e797
Weelam <wl.lin@mail.utoronto.ca>
2020-11-08 03:04:21 -0500

    Added methods in EventSignupPresenter (but still needs work)

--- phase1/src/EventSignupPresenter.java
+++ phase1/src/EventSignupPresenter.java
@@ -1,3 +1,27 @@
 public class EventSignupPresenter {
+    private EventManager em;
+    private EventSignup es;
 
+    public EventSignupPresenter(EventSignup es, EventManager em) {
+        this.es = es;
+        this.em = em;
+    }
+
+    public void joinEvent(User user, String event_name) {
+        EventSignupController esc = new EventSignupController(this.es, this.em);
+        if (!(esc.signUserUp(user, event_name))) {
+            System.out.println("Unable to join event");
+        } else {
+            System.out.println("Joined event");
+        }
+    }
+
+    public void leaveEvent(User user, String event_name) {
+        EventSignupController esc = new EventSignupController(this.es, this.em);
+        if (!(esc.removeUser(user, event_name))) {
+            System.out.println("Unable to leave event");
+        } else {
+            System.out.println("Left event");
+        }
+    }
 }
