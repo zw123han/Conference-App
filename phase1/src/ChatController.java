56a95f36c4141efc69f754b52e6cf620864c6e30
Weelam <wl.lin@mail.utoronto.ca>
2020-11-08 01:10:30 -0500

    changed the name of the controller to chat to ChatController

--- phase1/src/ChatController.java
+++ phase1/src/ChatController.java
@@ -1,11 +1,7 @@
 import java.util.ArrayList;
 
-public class canIMessageThisPersonIDontKnowCanYou {
+public class ChatController {
     Chatter chatter = new Chatter();
-
-    public canIMessageThisPersonIDontKnowCanYou() {
-    }
-
     public boolean canSendOne(User user, String recipient, Message message) {
         if ((user instanceof Attendee || user instanceof Organizer) && user.getFriends().contains(recipient)) {
             ArrayList<String> recipients = new ArrayList<>();
