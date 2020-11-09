b926dd9887d7ef6528004cbb1a3ea67fed9031ca
Chrisee Zhu <fishnuggets.zhu@mail.utoronto.ca>
2020-11-08 20:20:34 -0800

    completed controller methods for sending messages

--- phase1/src/ChatController.java
+++ phase1/src/ChatController.java
@@ -3,12 +3,32 @@ import java.util.ArrayList;
 public class ChatController {
     private Chatter chatter = new Chatter();
 
-    public boolean canSendOne(User user, String recipient, Message message) {
-        if ((user instanceof Attendee || user instanceof Organizer) && user.getFriends().contains(recipient)) {
+    private boolean isInEvent(User user, String recipient, EventManager em) {
+        for (Long evt : user.getEvents()) {
+            if (em.getEventById(evt).hasUser(recipient)) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public boolean canSendOne(User user, String recipient, Message message, EventManager em) {
+        if ((user instanceof Attendee && user.getFriends().contains(recipient)) ||
+                (user instanceof Organizer &&
+                        (user.getFriends().contains(recipient) || isInEvent(user, recipient, em)))){
             ArrayList<String> recipients = new ArrayList<>();
             recipients.add(user.getUserName());
             recipients.add(recipient);
             chatter.sendOne(recipients, message);
+            return true;
+        }
+        return false;
+    }
+
+    public boolean canSendAll(User user, Long evt, Message message, EventManager em) {
+        if ((user instanceof Organizer || user instanceof Speaker) && user.getEvents().contains(evt)) {
+            Event event = em.getEventById(evt);
+            chatter.sendAll(event, message);
             return true;
         }
         return false;
