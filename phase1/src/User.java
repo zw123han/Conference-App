19f8427964e1768b2c73b8e2aeac1f4d5ec28ac5
Jesse Na <jessena.2001@gmail.com>
2020-11-09 20:27:37 -0500

    Added a constructor for registrar and a getter for user

--- phase1/src/User.java
+++ phase1/src/User.java
@@ -1,7 +1,7 @@
 import java.util.ArrayList;
 
 public abstract class User{
-    private String name, userName, hashPassword;
+    private String name, userName, hashPassword, type;
 
     private ArrayList<Long> events;
     private ArrayList<String> friends;
@@ -16,6 +16,8 @@ public abstract class User{
         //Work in Progress. May be omitted in future.
         System.out.println("Error. Missing: name, user name and password");
     }
+
+    public String getType() { return type; }
 
     public String getName(){ return name; }
 
