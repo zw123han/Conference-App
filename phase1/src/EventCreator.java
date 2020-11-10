5e3aa13b43dc342384e6d5251f5a873793617b3c
Andy Wu <andyctct.wu@mail.utoronto.ca>
2020-11-10 12:21:18 -0800

    Added an event creation presenter and edited the event creation exception. Also updated the successful event creation criteria.

--- phase1/src/EventCreator.java
+++ phase1/src/EventCreator.java
@@ -8,16 +8,23 @@ public class EventCreator {
         this.em = em;
     }
 
-    public boolean createEvent(String name, String room, LocalDateTime time, Speaker speaker, int capacity) throws EventExistsException {
+    public boolean createEvent(String name, String room, LocalDateTime start_time, Speaker speaker, int capacity)
+            throws EventCreationFailureException {
         ArrayList<Event> events = this.em.getEventsList();
         for (Event event : events) {
-            if ((event.getRoom().equals(room) && event.getTime() == time)) {
-                throw new EventExistsException("this room is taken for this time");
-            } else if (event.getTime() == time && event.getSpeaker().getUserName().equals(speaker.getUserName())) {
-                throw new EventExistsException("this speaker is unavailable for this time");
+            LocalDateTime lower = event.getTime();
+            LocalDateTime upper = lower.plusMinutes(60);
+            LocalDateTime end_time = start_time.plusMinutes(60);
+            if ((start_time.isAfter(lower) && start_time.isBefore(upper)) ||
+                    (end_time.isAfter(lower) && end_time.isBefore(upper)) || (start_time.isEqual(lower))){
+                if ((event.getRoom().equals(room))) {
+                    throw new EventCreationFailureException("This room is taken for this time");
+                } else if (event.getSpeaker().getUserName().equals(speaker.getUserName())) {
+                    throw new EventCreationFailureException("This speaker is unavailable for this time");
+                }
             }
         }
-        this.em.createEvent(name, room, time, speaker, capacity);
+        this.em.createEvent(name, room, start_time, speaker, capacity);
         return true;
     }
 }
--- phase1/src/EventExistsException.java
+++
@@ -1,8 +0,0 @@
-public class EventExistsException extends Exception{
-    public EventExistsException() {
-        super("This event already exists");
-    }
-    public EventExistsException(String message) {
-        super(message);
-    }
-}
