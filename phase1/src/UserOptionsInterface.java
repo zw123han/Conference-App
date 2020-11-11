cee0b1e36091a0aeccd354b9f84aecb616021a45
Weelam <wl.lin@mail.utoronto.ca>
2020-11-11 14:09:39 -0500

    fixed a minor typo in UserOptionsInterface

--- phase1/src/UserOptionsInterface.java
+++ phase1/src/UserOptionsInterface.java
@@ -57,7 +57,7 @@ public class UserOptionsInterface {
                     System.out.println("Please input a valid option.");
                     break;
             }
-        } else if (user instanceof Speaker) {
+        } else {
             switch (choice) {
                 case "Logout":
                     logout();
