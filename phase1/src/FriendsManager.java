0178d457a967c2cf36154bc3dd1b778f556eb5f0
Chrisee Zhu <fishnuggets.zhu@mail.utoronto.ca>
2020-11-11 03:53:04 -0800

    added FriendsManager with todo (WIP)

---
+++ phase1/src/FriendsManager.java
@@ -0,0 +1,27 @@
+import java.util.*;
+
+public class FriendsManager {
+    private User user;
+    private ArrayList<String> allUsers; // TODO: implement getUserNames() in Registrar
+
+    public FriendsManager(User user, ArrayList<String> allUsers) {
+        this.user = user;
+        this.allUsers = allUsers;
+    }
+
+    public boolean addFriend(String username) {
+        if (!user.hasFriend(username) && allUsers.contains(username)) {
+            user.addFriend(username);
+            return true;
+        }
+        return false;
+    }
+
+    public boolean removeFriend(String username) {
+        if (user.hasFriend(username)) {
+            user.removeFriend(username);
+            return true;
+        }
+        return false;
+    }
+}
