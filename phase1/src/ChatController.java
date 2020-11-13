46f5847ff66470d892e1335bd60e6c82e9c16d68
FredS <fred.sallah@mail.utoronto.ca>
2020-11-12 22:09:04 -0500

    Inserted Javadoc template to ChatController class.

--- phase1/src/ChatController.java
+++ phase1/src/ChatController.java
@@ -1,5 +1,11 @@
 import java.util.ArrayList;
 
+/**
+ * (please describe)
+ *
+ * @author
+ * @version %I%, %G%
+ */
 public class ChatController {
 
     private boolean isInEvent(User user, String recipient, EventManager em) {
@@ -11,19 +17,49 @@ public class ChatController {
         return false;
     }
 
+    /**
+     * (please describe)
+     *
+     * @param message       (please describe)
+     * @return              (please describe)
+     */
     public boolean validateMessage(String message) {
         return message.length() != 0;
     }
 
+    /**
+     * (please describe)
+     *
+     * @param user          (please describe)
+     * @param recipient     (please describe)
+     * @param reg           (please describe)
+     * @return              True or false.
+     */
     public boolean canMessage(User user, String recipient, Registrar reg) {
         return ((user.hasFriend(recipient) || user instanceof Organizer) && reg.userExisting(recipient));
     }
 
+    /**
+     * (please describe)
+     *
+     * @param user          (please describe)
+     * @param evt           (please describe)
+     * @param em            (please describe)
+     * @return              True of false.
+     */
     public boolean canMessage(User user, Long evt, EventManager em) {
         return em.hasEvent(evt) ||
                 (em.hasEvent(evt) && user instanceof Speaker && em.getEventById(evt).getSpeaker().equals(user.getName()));
     }
 
+    /**
+     * (please describe)
+     *
+     * @param user          (please describe)
+     * @param recipient     (please describe)
+     * @param cm            (please describe)
+     * @return              True of false.
+     */
     public boolean canReply(User user, String recipient, ChatroomManager cm) {
         if (cm.hasChatroom(user, recipient)) {
             if (user instanceof Organizer || user instanceof Attendee) {
@@ -39,6 +75,13 @@ public class ChatController {
         return false;
     }
 
+    /**
+     * (please describe)
+     *
+     * @param user          (please describe)
+     * @param recipient     (please describe)
+     * @param message       (please describe)
+     */
     public void sendMessage(User user, String recipient, String message) {
         ChatPull pull = new ChatPull();
         ChatroomManager cm = pull.readChatlog();
@@ -51,6 +94,14 @@ public class ChatController {
         push.storeChat(cm);
     }
 
+    /**
+     * (please describe)
+     *
+     * @param user          (please describe)
+     * @param evt           (please describe)
+     * @param message       (please describe)
+     * @param em            (please describe)
+     */
     public void sendMessage(User user, Long evt, String message, EventManager em) {
         ChatPull pull = new ChatPull();
         ChatroomManager cm = pull.readChatlog();
