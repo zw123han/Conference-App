16e45ea563a7fb281def8a01239beb11a83967ce
Weelam <wl.lin@mail.utoronto.ca>
2020-11-12 20:45:15 -0500

    removed some unnecessary imports

--- phase1/src/EventCreatorPresenter.java
+++ phase1/src/EventCreatorPresenter.java
@@ -1,7 +1,4 @@
 import java.time.LocalDateTime;
-import java.time.format.DateTimeFormatter;
-import java.time.format.DateTimeParseException;
-import java.util.InputMismatchException;
 
 public class EventCreatorPresenter {
     private EventManager em;
