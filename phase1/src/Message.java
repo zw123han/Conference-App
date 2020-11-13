9c5b00b77b88608a21f38f1418c04b7594039bee
FredS <fred.sallah@mail.utoronto.ca>
2020-11-13 00:52:08 -0500

    Created Javadoc template for methods in Message

--- phase1/src/Message.java
+++ phase1/src/Message.java
@@ -12,19 +12,40 @@ public class Message implements Serializable {
     private String message, sender;
     private ZonedDateTime time = ZonedDateTime.now();
 
+    /**
+     * (please describe)
+     *
+     * @param message       (please describe)
+     * @param sender        (please describe)
+     */
     public Message(String message, String sender) {
         this.message = message;
         this.sender = sender;
     }
 
+    /**
+     * (please describe)
+     *
+     * @return      (please describe)
+     */
     public String getMessage() {
         return message;
     }
 
+    /**
+     * (please describe)
+     *
+     * @return      (please describe)
+     */
     public String getSender() {
         return sender;
     }
 
+    /**
+     * (please describe)
+     *
+     * @return      (please describe)
+     */
     public String getDate() {
         DateTimeFormatter d = DateTimeFormatter.ofPattern("dd/MM/yyyy HH:mm");
         return time.toLocalDateTime().format(d);
