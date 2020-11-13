d05b305ea6ffde19063b5ec83e51fd678e79a833
FredS <fred.sallah@mail.utoronto.ca>
2020-11-12 23:22:49 -0500

    Updated method Javadoc template to EventSignup

--- phase1/src/EventSignup.java
+++ phase1/src/EventSignup.java
@@ -9,12 +9,23 @@ import java.time.*;
 // Choosing not to store event manager in here for now
 public class EventSignup{
 
-
+    /**
+     * (please describe)
+     *
+     * @param user      (please describe)
+     * @param event     (please describe)
+     */
     public void signUserUp(User user, Event event){
         user.addEvent(event.getId());
         event.addUser(user.getUserName());
     }
 
+    /**
+     * (please describe)
+     *
+     * @param user      (please describe)
+     * @param event     (please describe)
+     */
     public void removeUser(User user, Event event){
         user.removeEvent(event.getId());
         event.removeUser(user.getUserName());
