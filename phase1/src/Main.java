e917a282f14424efcd62d6a3002b87174f6fe038
FredS <fred.sallah@mail.utoronto.ca>
2020-11-13 00:47:24 -0500

    Created Javadoc template for methods in Main

--- phase1/src/Main.java
+++ phase1/src/Main.java
@@ -1,11 +1,16 @@
 /**
- * (please describe)
+ * Main static method of program.
  *
- * @author
+ * @author Fred
  * @version %I%, %G%
  */
 public class Main {
 
+    /**
+     * This is the main static method of the program.
+     *
+     * @param arg unused arguments. Here by convention.
+     */
     public static void main(String[] arg){
 
         ConferenceSimulator conference = new ConferenceSimulator();
