b34e0e247ac69e3fd49d2298a7df3b8c41e2bf98
FredS <fred.sallah@mail.utoronto.ca>
2020-11-13 00:30:24 -0500

    Created Javadoc template for methods in LoginOptionsFacade

--- phase1/src/LoginOptionsFacade.java
+++ phase1/src/LoginOptionsFacade.java
@@ -12,30 +12,69 @@ public class LoginOptionsFacade {
     private Login login;
     private User user;
 
+    /**
+     * (please describe)
+     *
+     * @param registrar     (please describe)
+     */
     public LoginOptionsFacade(Registrar registrar){
         this.credentialsUseCase = new CredentialsUseCase(registrar);
         this.login = new Login(registrar);
         this.user = null;
     }
 
-
+    /**
+     * (please describe)
+     *
+     * @param name          (please describe)
+     * @param username      (please describe)
+     * @param password      (please describe)
+     * @param type          (please describe)
+     * @return              True or false.
+     */
     public boolean createUser(String name, String username, String password, String type){
         return credentialsUseCase.createUser(name, username, password, type);
     }
 
+    /**
+     * (please describe)
+     *
+     * @param username              (please describe)
+     * @param currentPassword       (please describe)
+     * @param newPassword           (please describe)
+     * @return                      True or false.
+     */
     public boolean resetPassword(String username, String currentPassword, String newPassword) {
         return credentialsUseCase.resetPassword(username, currentPassword, newPassword);
     }
 
+    /**
+     * (please describe)
+     *
+     * @param username      (please describe)
+     * @param password      (please describe)
+     * @return              True or false.
+     */
     public boolean login(String username, String password){
         this.user = login.attemptLogin(username, password);
         return this.user != null;
     }
 
+    /**
+     * (please describe)
+     *
+     * @param username      (please describe)
+     * @return              True or false.
+     */
     public boolean userExists(String username) {
         return login.userExists(username);
     }
 
+    /**
+     * (please describe)
+     *
+     * @return      True or false.
+     */
     public boolean logout(){
         if (this.user==null){
             return false;
@@ -44,6 +83,11 @@ public class LoginOptionsFacade {
         return true;
     }
 
+    /**
+     * (please describe)
+     *
+     * @return      (please describe)
+     */
     public User getUser(){
         return this.user;}
 }
