4efb5a1e99a4e31aced08726efe3482ed5e220a4
FredS <fred.sallah@mail.utoronto.ca>
2020-11-13 00:53:15 -0500

    Created Javadoc template for methods in Organizer

--- phase1/src/Organizer.java
+++ phase1/src/Organizer.java
@@ -6,6 +6,13 @@
  */
 public class Organizer extends User {
 
+    /**
+     * (please describe)
+     *
+     * @param name          (please describe)
+     * @param userName      (please describe)
+     * @param password      (please describe)
+     */
     public Organizer(String name, String userName, String password) {
         super(name, userName, password);
     }
