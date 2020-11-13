58fe5fbdf0d4dee8c663f15325cad0bcab5ba300
FredS <fred.sallah@mail.utoronto.ca>
2020-11-13 01:21:18 -0500

    Created Javadoc template for methods in SaveEvents

--- phase1/src/SaveEvents.java
+++ phase1/src/SaveEvents.java
@@ -11,11 +11,21 @@ import java.util.List;
 public class SaveEvents {
     private String filepath;
 
+    /**
+     * (please describe)
+     *
+     * @param filepath      (please describe)
+     */
     public SaveEvents(String filepath) {
         this.filepath = filepath;
 
     }
 
+    /**
+     * (please describe)
+     *
+     * @param evList        (please describe)
+     */
     public void saveEvents(ArrayList<Event> evList) {
         try {
             FileOutputStream streamOut = new FileOutputStream(this.filepath);
