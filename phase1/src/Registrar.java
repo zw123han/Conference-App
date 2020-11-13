f4da251ce8072fe250b8adfaf1e65a06205a76a7
FredS <fred.sallah@mail.utoronto.ca>
2020-11-12 23:03:53 -0500

    Updated method Javadoc template to Registrar

--- phase1/src/Registrar.java
+++ phase1/src/Registrar.java
@@ -11,16 +11,31 @@ public class Registrar {
 
     private ArrayList<User> users;
 
-    //Constructor used when loading from a file
+    /**
+     * Constructor used when loading from a file
+     *
+     * @param users     (please describe)
+     */
     public Registrar(ArrayList<User> users) {
         this.users = users;
     }
 
-    //Constructor used when not loading from a file
+    /**
+     * Constructor used when not loading from a file
+     */
     public Registrar() {
         this.users = new ArrayList<>();
     }
 
+    /**
+     * (please describe)
+     *
+     * @param name          (please describe)
+     * @param userName      (please describe)
+     * @param password      (please describe)
+     * @param type          (please describe)
+     * @return              (please describe)
+     */
     public boolean createUser(String name, String userName, String password, String type) {
         User user;
         if (type.toLowerCase().equals("attendee")) {
@@ -36,6 +51,12 @@ public class Registrar {
         return true;
     }
 
+    /**
+     * (please describe)
+     *
+     * @param userName      (please describe)
+     * @return              (please describe)
+     */
     public User getUserByUserName(String userName) {
         for (User user : users) {
             if (user.getUserName().equals(userName)) {
@@ -45,15 +66,32 @@ public class Registrar {
         return null;
     }
 
+    /**
+     * (please describe)
+     *
+     * @param userName      (please describe)
+     * @return              (please describe)
+     */
     public String getUserType(String userName) {
         User user = getUserByUserName(userName);
         return user.getClass().getName();
     }
 
+    /**
+     * (please describe)
+     *
+     * @return              (please describe)
+     */
     public ArrayList<User> getUsers() {
         return users;
     }
 
+    /**
+     * (please describe)
+     *
+     * @param type          (please describe)
+     * @return              (please describe)
+     */
     public ArrayList<User> getUsersByType(String type) {
         ArrayList<User> users = new ArrayList<>();
         for (User user : this.users) {
@@ -64,10 +102,21 @@ public class Registrar {
         return users;
     }
 
+    /**
+     * (please describe)
+     *
+     * @param username      (please describe)
+     * @return              True of false.
+     */
     public boolean userExisting(String username){
         return getUserByUserName(username) != null;
     }
 
+    /**
+     * (please describe)
+     *
+     * @return              (please describe)
+     */
     public ArrayList<String> getUserNames(){
         ArrayList<String> allUsernames = new ArrayList<>();
         for (User user : users){
