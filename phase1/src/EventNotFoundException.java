949f5e7eb6245ddbad11a4e55ab8fc9af9169e06
FredS <fred.sallah@mail.utoronto.ca>
2020-11-12 23:15:58 -0500

    Updated method Javadoc template to EventNotFoundException

--- phase1/src/EventNotFoundException.java
+++ phase1/src/EventNotFoundException.java
@@ -5,9 +5,18 @@
  * @version %I%, %G%
  */
 public class EventNotFoundException extends Exception {
+    /**
+     * (please describe)
+     *
+     * @param message       (please describe)
+     */
     public EventNotFoundException(String message){
         super(message);
     }
+
+    /**
+     * (please describe)
+     */
     public EventNotFoundException(){
         super("This event has not yet been registered.");
     }
