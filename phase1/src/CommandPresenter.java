95a984262495241bbb7301a0af378aa9c4c8bf11
FredS <fred.sallah@mail.utoronto.ca>
2020-11-12 22:25:10 -0500

    Inserted method Javadoc template to ChatroomManager.

--- phase1/src/CommandPresenter.java
+++ phase1/src/CommandPresenter.java
@@ -7,14 +7,28 @@ import java.util.*;
  * @version %I%, %G%
  */
 public abstract class CommandPresenter {
+
+    /**
+     * (please describe)
+     */
     public void exitMessage() {
         System.out.println("$q to exit.");
     }
 
+    /**
+     * (please describe)
+     *
+     * @param field     (please describe)
+     */
     public void invalidCommand(String field) {
         System.out.println(field + " not valid.");
     }
 
+    /**
+     * (please describe)
+     *
+     * @param field     (please describe)
+     */
     public void commandPrompt(String field) {
         System.out.println("Enter a valid " + field + ".");
         exitMessage();
