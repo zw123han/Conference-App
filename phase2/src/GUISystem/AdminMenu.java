38ac7df16c619f485f6a100285f524e940c9dff3
Weelam <wl.lin@mail.utoronto.ca>
2020-12-04 02:31:47 -0500

    changed the name of a file

--- phase2/src/GUISystem/AdminMenu.java
+++ phase2/src/GUISystem/AdminMenu.java
@@ -11,7 +11,7 @@ import javafx.stage.Stage;
 
 import java.io.IOException;
 
-public class HomeMenuBrain {
+public class AdminMenu {
 
     private EventMenu eventMenu;
     private MessageMenu messageMenu;
