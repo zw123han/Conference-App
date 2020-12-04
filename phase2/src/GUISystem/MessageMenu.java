4d16b93e52dcbf771813e0febcd36be087e291ac
Chrisee Zhu <fishnuggets.zhu@mail.utoronto.ca>
2020-12-03 23:16:27 -0800

    "+" new message button now exists

--- phase2/src/GUISystem/MessageMenu.java
+++ phase2/src/GUISystem/MessageMenu.java
@@ -28,27 +28,40 @@ public class MessageMenu extends Application implements MessageInboxUI.IView {
     @Override
     public void start(Stage primaryStage) {
         // MESSAGE INBOX CONTAINER
-        HBox InboxCanvas = new HBox();
-        InboxCanvas.setAlignment(Pos.CENTER_LEFT);
+        HBox inboxCanvas = new HBox();
+        inboxCanvas.setAlignment(Pos.CENTER_LEFT);
 
         // CHATROOM OPTIONS CONTAINER
         VBox chatroomCanvas = new VBox();
         chatroomCanvas.setAlignment(Pos.TOP_CENTER);
         // CHILD #1: TITLE BAR
-        chatroomCanvasTitle = new Label("Chats");
+        HBox chatroomBar = new HBox(10);
+        chatroomBar.setAlignment(Pos.CENTER_LEFT);
+        chatroomBar.setPrefSize(180, 40);
+        // New Message Button
+        Button newMessage = new Button("+");
+        newMessage.setPrefSize(20, 20);
+        newMessage.setFont(Font.loadFont(getClass().getResourceAsStream("/open-sans/os-extrabold.ttf"), 12));
+        newMessage.setBackground(new Background(new BackgroundFill(Color.WHITE, new CornerRadii(10), new Insets(0))));
+        newMessage.setPadding(new Insets(0, 6, 0, 6));
+        // Chatroom Title
+        chatroomCanvasTitle = new Label("Inbox");
         chatroomCanvasTitle.setFont(Font.loadFont(getClass().getResourceAsStream("/open-sans/os-bold.ttf"), 16));
         chatroomCanvasTitle.setTextFill(Color.WHITE);
         Background chatroomCanvasBackground = new Background(new BackgroundFill(Color.BLACK, new CornerRadii(0), new Insets(0)));
         chatroomCanvas.setBackground(chatroomCanvasBackground);
         chatroomCanvasTitle.setPadding(new Insets(10));
-        chatroomCanvasTitle.setPrefSize(180, 40);
+        chatroomCanvasTitle.setPrefSize(140, 40);
+        // Putting everything into chatroomBar
+        chatroomBar.getChildren().addAll(chatroomCanvasTitle,newMessage);
         // CHILD #2: SCROLLABLE CHATROOM OPTIONS
         ScrollPane chatroomOptionsScrollable = new ScrollPane();
         chatroomOptions = new VBox();
         chatroomOptions.setPadding(new Insets(10));
         chatroomOptionsScrollable.setPrefSize(180, 540);
         chatroomOptionsScrollable.setContent(chatroomOptions);
-        chatroomCanvas.getChildren().addAll(chatroomCanvasTitle, chatroomOptionsScrollable);
+        // PUTTING EVERYTHING INTO CHATROOM CANVAS
+        chatroomCanvas.getChildren().addAll(chatroomBar, chatroomOptionsScrollable);
 
         // CHAT MESSAGES CONTAINER
         VBox messageCanvas = new VBox();
@@ -66,36 +79,34 @@ public class MessageMenu extends Application implements MessageInboxUI.IView {
         messagesScrollable.setVvalue(1.0);
         // CHILD #3: MESSAGE TEXT AREA
         HBox messageBar = new HBox(5);
-        messageBar.setPrefSize(320, 100);
+        messageBar.setPrefSize(320, 160);
         messageBar.setAlignment(Pos.BOTTOM_LEFT);
         messageBar.setPadding(new Insets(5));
         // Message Box (Field)
         messageBox = new TextArea();
         messageBox.setFont(Font.loadFont(getClass().getResourceAsStream("/open-sans/os-regular.ttf"), 12));
         messageBox.setPadding(new Insets(4));
-        messageBox.setPrefSize(260, 40);
+        messageBox.setPrefSize(260, 160);
         messageBox.setWrapText(true);
-
         // Send Message Button
         Button sendMessage = new Button("SEND");
         sendMessage.setPrefSize(50, 30);
         sendMessage.setFont(Font.loadFont(getClass().getResourceAsStream("/open-sans/os-bold.ttf"), 10));
-        CornerRadii corn = new CornerRadii(10);
-        Background background = new Background(new BackgroundFill(Color.BLACK, corn, new Insets(0)));
-        sendMessage.setBackground(background);
+        sendMessage.setBackground(new Background(new BackgroundFill(Color.BLACK, new CornerRadii(10), new Insets(0))));
         sendMessage.setTextFill(Color.WHITE);
         sendMessage.setPadding(new Insets(8));
-        sendMessage.setOnAction(e -> sendMessage(messageBox.getText()));
+        sendMessage.setOnAction(e -> sendMessage(messageBox));
+        // Putting everything into MessageBar
         messageBar.getChildren().addAll(messageBox, sendMessage);
-
+        // PUTTING EVERYTHING INTO MESSAGECANVAS
         messageCanvas.getChildren().addAll(messageCanvasTitle, messagesScrollable, messageBar);
 
         // PRELIMINARY VIEW LOADING
         //mi.loadChatroomCanvasView();
 
         // SCENE AND STAGE
-        InboxCanvas.getChildren().addAll(chatroomCanvas, messageCanvas);
-        Scene scene = new Scene(InboxCanvas, 500, 600);
+        inboxCanvas.getChildren().addAll(chatroomCanvas, messageCanvas);
+        Scene scene = new Scene(inboxCanvas, 500, 600);
         primaryStage.setTitle("Messages - Conference Simulator Phase 2");
         primaryStage.setScene(scene);
         primaryStage.setMinHeight(500);
@@ -106,7 +117,7 @@ public class MessageMenu extends Application implements MessageInboxUI.IView {
     }
 
     public void setChatroomCanvasTitle(String numberUnread) {
-        chatroomCanvasTitle.setText("Chats (Unread: " + numberUnread + ")");
+        chatroomCanvasTitle.setText("Inbox (" + numberUnread + ")");
     }
 
     public void setChatroomOption(ArrayList<String> option) {
@@ -128,9 +139,10 @@ public class MessageMenu extends Application implements MessageInboxUI.IView {
         Label unread = new Label();
         if (!option.get(2).equals("0")) {
             unread.setText(option.get(2));
-            unread.setFont(Font.loadFont(getClass().getResourceAsStream("/open-sans/os-regular.ttf"), 12));
+            unread.setFont(Font.loadFont(getClass().getResourceAsStream("/open-sans/os-extrabold.ttf"), 10));
             CornerRadii corn = new CornerRadii(10);
-            Background background = new Background(new BackgroundFill(Color.BLACK, corn, new Insets(2)));
+            Background background = new Background(new BackgroundFill(Color.BLACK, corn, new Insets(0, 6, 0, 6)));
+            unread.setPrefHeight(16);
             unread.setBackground(background);
         }
         chatroomOptionContainer.getChildren().addAll(displayName, username, unread);
@@ -173,8 +185,9 @@ public class MessageMenu extends Application implements MessageInboxUI.IView {
         messageDisplay.getChildren().add(messageContainer);
     }
 
-    public void sendMessage(String message) {
-        mi.sendMessage(message, recipient);
+    public void sendMessage(TextArea messageBox) {
+        messageBox.setText("");
+        mi.sendMessage(messageBox.getText(), recipient);
     }
 
     public void display(Stage primaryStage) {
