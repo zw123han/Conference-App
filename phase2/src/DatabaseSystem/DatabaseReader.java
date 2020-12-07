0384b63895db38154d802456d3062c454e375fcd
Jesse Na <jessena.2001@gmail.com>
2020-12-06 20:46:01 -0500

    Retrieve profanity list from database now

--- phase2/src/DatabaseSystem/DatabaseReader.java
+++ phase2/src/DatabaseSystem/DatabaseReader.java
@@ -2,6 +2,10 @@ package DatabaseSystem;
 
 import com.mongodb.DB;
 import com.mongodb.DBCollection;
+import com.mongodb.DBCursor;
+import com.mongodb.DBObject;
+
+import java.util.HashMap;
 
 public class DatabaseReader {
 
@@ -10,6 +14,19 @@ public class DatabaseReader {
         DBCollection collection = db.getCollection(sv.getCollectionName());
 
         return strategy.parseCollection(collection);
+    }
+
+    public HashMap<String, String> getProfanityList(DB db) {
+        HashMap<String, String> profanities = new HashMap<>();
+
+        DBCollection collection = db.getCollection("profanities");
+        DBCursor cursor = collection.find();
+        while (cursor.hasNext()) {
+            DBObject doc = cursor.next();
+            profanities.put((String) doc.get("regex"), (String) doc.get("replacement"));
+        }
+
+        return profanities;
     }
 
 }
