67d2bfb1bc8aca679ce96ecf41bfc6432c0c7341
zw123han <ziwen.han@mail.utoronto.ca>
2020-12-07 05:16:47 -0500

    Fixed text issue in login

--- phase2/src/ConferenceBuilder.java
+++ phase2/src/ConferenceBuilder.java
@@ -1,4 +1,14 @@
+import EventSystem.EventManager;
+import MessagingSystem.ChatroomManager;
+import UserSystem.*;
+
+import java.util.HashMap;
+
 public class ConferenceBuilder {
 
     // Constructs the application necessary for conference simulator to run
+
+    public void BuildAConference(Registrar registrar, EventManager eventManager, ChatroomManager chatroomManager, HashMap<String, String> profanities){
+
+    }
 }
